623f89b52b69d633a5ce231827e441e65e02a70b
Dwi Susanto <wis4nto@gmail.com>
2024-07-23 22:56:18 +0700

    anj

--- src/emails/emails.controller.ts
+++ src/emails/emails.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, Query, Res } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Res } from '@nestjs/common'
 import { EmailsService } from './emails.service'
 import { EmailFilterQueryDto } from './dtos/email-filter-query.dto'
 
@@ -11,7 +11,7 @@ export class EmailsController {
 		return this.emailsService.findAll(res.locals.identity, dto)
 	}
 
-	@Get('/')
+	@Delete('/')
 	async deleteAll(@Res({ passthrough: true }) res: any, @Query() dto: EmailFilterQueryDto) {
 		return this.emailsService.deleteAll(res.locals.identity, dto)
 	}
